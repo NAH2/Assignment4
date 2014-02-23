b52211d8549246fcd0ccf9bde59ff4a1b7b8fe7d
rubbish9529 <rubbish9529@yahoo.com.hk>
2014-02-23 05:10:38 +0000

    Update SelectGame.java

--- GUIDisplay/SelectGame.java
+++ GUIDisplay/SelectGame.java
@@ -1,8 +1,9 @@
 import javax.swing.*;
+import java.io.IOException;
 
 //public class SelectGame extends GUIDisplay {
 public class SelectGame {
-	private String[] game = {"Othello", "ConnectFour"};
+	private String[] game = {"Othello", "Connect4"};
 	private String chosenGame;
 
 	private boolean SetGame(String chosenGame) {
@@ -16,14 +17,25 @@ public class SelectGame {
 	
 	public boolean draw() {	
 		chosenGame = JOptionPane.showInputDialog(null,"GAME", "Choose a game to play", JOptionPane.INFORMATION_MESSAGE, null, game, game[0])+""; //return an object so add empty string
+		try {
+			if(chosenGame.equals("Othello")){
+				GameBoard GB = new GameBoard(true);
+				GB.setVisible(true);
+				//Selection SE = new Selection(chosenGame);
+			} else {
+				GameBoard GB = new GameBoard(false);
+				//Selection SE = new Selection(chosenGame);
+			}
+		} catch (IOException e) {}
+
 		return true;
 	}
 
 	//public boolean clear() {
 	//}
-	/*public static void main(String[] args) {
+
+	public static void main(String[] args) {
 		SelectGame a = new SelectGame();
 		a.draw();
-		System.out.print
-	}*/
+	}
 }
