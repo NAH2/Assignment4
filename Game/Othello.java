1aa8cf11e9ad980061fb4d55127c3b7f550a6540
rubbish9529 <rubbish9529@yahoo.com.hk>
2014-02-23 06:19:24 +0000

    Update Othello.java

--- Game/Othello.java
+++ Game/Othello.java
@@ -6,6 +6,7 @@ import java.awt.Color;
 	private int scoreWhite, scoreBlack, boardSize = 8, startingScore = 0;
 	private boolean playerTurn;
 	private int empty = 0,black = 1, white = 2, counter;
+	private int[] flipdata;
 	
 	public boolean SetPlayer(boolean playerTurn) {
 		this.playerTurn = playerTurn;
@@ -53,7 +54,7 @@ import java.awt.Color;
 	        for (int i = 0; i < 8; i++) {
 	            for (int j = 0; j < 8; j++) {
 					//availableMov[i][j] = 'X';
-	                if(ValidMove(i,j,gameBoard,playerTurn)){
+	                if( ValidMove(i,j,gameBoard,playerTurn)){
 	                    availableMov[i][j] = 'O';   //Location of available Move 
 	                }
 	                else 
@@ -62,7 +63,7 @@ import java.awt.Color;
 	        }
 	        return availableMov;
 	        
-	}
+	    }
 	
 	/*public boolean ChangePieces(int[][] newPiecePosition) {
 	}
@@ -75,11 +76,11 @@ import java.awt.Color;
 	        if (ValidMove(y, x, gameBoard, playerTurn)) {
 	            if (playerTurn == true) {
 	                gameBoard[y][x] = black;
-					gameBoard = Flip(y, x, gameBoard, playerTurn);
+					gameBoard = Flip(flipdata, gameBoard);
 	                return gameBoard;
 	            } else {
 	                gameBoard[y][x] = white;
-					gameBoard = Flip(y, x, gameBoard, playerTurn);
+					gameBoard = Flip(flipdata, gameBoard);
 	                return gameBoard;
 	            }
 	        } 
@@ -97,6 +98,7 @@ import java.awt.Color;
 		            } else {
 		                playerColor = white;
 		            }
+					counter = 1;
 
 		            // Search each direction (8 direction)
 		            for (int i = -1; i <= 1; i++) {
@@ -127,6 +129,7 @@ import java.awt.Color;
 		                        //prevent out of bound, if over 8, exit the while loop and stop search this direction
 		                        if ((searchX >= 8 || searchX < 0) || (searchY >= 8 || searchY < 0)) {
 		                            found = true;
+									counter = 0;
 		                        } else {
 		                            searchValue = gameBoard[searchY][searchX];
 		                        }
@@ -140,12 +143,13 @@ import java.awt.Color;
 										searchY -= i;
 										gameBoard[searchY][searchX] = playerColor;
 									}*/
+									flipdata = new int[] {searchY,searchX,i,j,counter,playerColor};
 		                            return true;
 		                        } //If end of the direction is empty , then stop search this direction.
 		                        else if (searchValue == empty) {
 		                            found = true;
 		                            //System.out.println("empty : true, Y:" + searchY + ", X:" + searchX);
-		                        } //counter++;
+		                        } counter++;
 		                    }
 		                }
 
@@ -158,74 +162,16 @@ import java.awt.Color;
 
 		    }
 		
-		public int[][] Flip(int y, int x, int[][] gameBoard, boolean playerTurn) {
-
-			            int searchX, searchY;
-			            int searchValue, playerColor;
-
-			            if (playerTurn == true) {
-			                playerColor = black;
-			            } else {
-			                playerColor = white;
-			            }
-						counter = 1;
-			            // Search each direction (8 direction)
-			            for (int i = -1; i <= 1; i++) {
-			                for (int j = -1; j <= 1; j++) {
-			                    boolean found = false;
-			                    searchY = y + i;
-			                    searchX = x + j;
-			                    if ((searchX >= 8 || searchX < 0) || (searchY >= 8 || searchY < 0)) {
-			                            System.out.println("Over bound! Y:" + searchY +" X: " +searchX + " i:" + i +" j: " + j);
-			                        continue;
-			                        
-			                    } else {
-			                        searchValue = gameBoard[searchY][searchX];
-			                    }
-			                    // skip the search if i and j is 0.
-			                    // skip the search if one of the direction is empty
-			                    // skip the search if one of the direction is same color piece
-			                    if ((i == 0 && j == 0) || searchValue == empty || searchValue == playerColor) {
-			                        continue;
-			                    }
-			
-			                    //Search along the direction
-			                    while (!found) {
-			                        System.out.println("Looping..");
-			                        searchX += j;
-			                        searchY += i;
-			                        //prevent out of bound, if over 8, exit the while loop and stop search this direction
-			                        if ((searchX >= 8 || searchX < 0) || (searchY >= 8 || searchY < 0)) {
-			                            found = true;
-			                        } else {
-			                            searchValue = gameBoard[searchY][searchX];
-			                        }
-			                        // if found the same color is along to direction 
-			                        if (searchValue == playerColor) {
-			                            found = true;
-			                            System.out.println("found : true, Y:" + searchY + ", X:" + searchX + ", newp value:" + searchValue);
-										System.out.println("FLIP "+counter+" PIECE(S)");
-										for(int a = 0; a<counter; a++){
-											searchX -= j;
-											searchY -= i;
-											gameBoard[searchY][searchX] = playerColor;
-										}
-			                            return gameBoard;
-			                        } //If end of the direction is empty , then stop search this direction.
-			                        else if (searchValue == empty) {
-			                            found = true;
-										//return gameBoard;
-			                            //System.out.println("empty : true, Y:" + searchY + ", X:" + searchX);
-			                        } counter++;
-			                    }
-			                }
-
-			            }
-			            //System.out.println("false");
-			            //return gameBoard;
-			        return gameBoard;
-
-			    }
+		public int[][] Flip(int[] flipdata, int[][] gameBoard) {
+			System.out.println("FLIP "+flipdata[4]+" PIECE(S)");
+				for(int a = 0; a<flipdata[4]; a++){
+					System.out.println(flipdata[4]);
+					flipdata[1] -= flipdata[3];
+					flipdata[0] -= flipdata[2];
+					gameBoard[flipdata[0]][flipdata[1]] = flipdata[5];
+				}
+			    return gameBoard;
+		}
 		
 	//change to string as output
 	/*public String WinningCondition(int[][] currentGameBoard) {
