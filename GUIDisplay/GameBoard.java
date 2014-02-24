61a15eec1f49a78d91b46fe937ef001cfc537676
modsz <supermodsz@gmail.com>
2014-02-24 01:42:12 +0000

    added comment

--- GUIDisplay/GameBoard.java
+++ GUIDisplay/GameBoard.java
@@ -1,4 +1,5 @@
-/**
+
+/*/**
  * @file    -GameBoard.java
  * @author  -Chun Kit So
  * @date    -21 Feb '2014
@@ -11,21 +12,17 @@ import java.awt.*;
 import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.event.*;
-import java.awt.image.BufferedImage;
-import javax.imageio.ImageIO;
-import java.io.File;
 import java.io.IOException;
 
 public class GameBoard extends javax.swing.JFrame {
 
     int[][] gameBoard;
     boolean playerTurn;
-    Image piece ;
-    private int empty = 0,black = 1, white = 2, boardSizeX, boardSizeY;
+    private int empty = 0, black = 1, white = 2, boardSizeX, boardSizeY;
     Othello othelloGame = new Othello();
     Icon blackpiece = new ImageIcon(getClass().getResource("black.png"));
     Icon whitepiece = new ImageIcon(getClass().getResource("white.png"));
-	private JPanel[][] squareBoard;
+    private JPanel[][] squareBoard;
 
     /**
      * Creates new form GameBoard
@@ -34,116 +31,116 @@ public class GameBoard extends javax.swing.JFrame {
         initComponents();
     }
 
-    public int[][] getGameboard (){
+    public int[][] getGameboard() {
         return this.gameBoard;
     }
-    
-    public void setGameboard(int[][] gameboard){
-         this.gameBoard = gameboard;
+
+    public void setGameboard(int[][] gameboard) {
+        this.gameBoard = gameboard;
     }
-    
-    public GameBoard(boolean game) throws IOException{     //true = othello , false = connect4 
-      initComponents();
-      
+
+    public GameBoard(boolean game) throws IOException {     //true = othello , false = connect4 
+        initComponents();
+
         if (game == true) {
-			boardSizeY = 8;
-			boardSizeX = 8;
-	 		squareBoard	= new JPanel[boardSizeY][boardSizeX];
-			//final JPanel square = new JPanel();
+            boardSizeY = 8;
+            boardSizeX = 8;
+            squareBoard = new JPanel[boardSizeY][boardSizeX];
             gameBoard = new int[boardSizeY][boardSizeX];
-            for (int i = 0; i < boardSizeY; i++) {
+            for (int i = 0; i < boardSizeY; i++) {    //use for loop to make a 8*8 game board 
                 for (int j = 0; j < boardSizeX; j++) {
                     Border blackBorder = BorderFactory.createLineBorder(Color.black);
-                    final JPanel square = new JPanel();
-					squareBoard[i][j] = square;
+                    final JPanel square = new JPanel();   // assign a Jpanel in each square
+                    squareBoard[i][j] = square;
                     square.setBorder(blackBorder);
-			        square.setBackground(new Color(170, 150, 100));
-					if((i == 3 && j == 3)||(i == 4 && j == 4)){
-						square.add(new JLabel(blackpiece));
-						gameBoard[i][j] = black;
-					} else if((i == 4 && j == 3)||(i == 3 && j == 4)){
-						square.add(new JLabel(whitepiece));
-						gameBoard[i][j] = white;
-					/*} else if((i == 2 && j == 2)||(i == 5 && j == 1)||(i == 6 && j ==3)||(i == 6 && j == 6)||(i == 4 && j == 6)||(i == 5 && j == 3)||(i == 3 && j == 5)){
-						square.add(new JLabel(whitepiece));
-						gameBoard[i][j] = white;*/
-					} else {
-						gameBoard[i][j] = empty;
-					}
+                    square.setBackground(new Color(170, 150, 100));
+                    if ((i == 3 && j == 3) || (i == 4 && j == 4)) {       // initial the game board, start with 2 black pieces and 2 white piece
+                        square.add(new JLabel(blackpiece));
+                        gameBoard[i][j] = black;
+                    } else if ((i == 4 && j == 3) || (i == 3 && j == 4)) {
+                        square.add(new JLabel(whitepiece));
+                        gameBoard[i][j] = white;
+                        /*} else if((i == 2 && j == 2)||(i == 5 && j == 1)||(i == 6 && j ==3)||(i == 6 && j == 6)||(i == 4 && j == 6)||(i == 5 && j == 3)||(i == 3 && j == 5)){
+                         square.add(new JLabel(whitepiece));
+                         gameBoard[i][j] = white;*/
+                    } else {
+                        gameBoard[i][j] = empty;
+                    }
+
                     gamePanel.add(square);
                     final int row = i;
                     final int col = j;
-                    square.addMouseListener(new MouseAdapter() {
+                    square.addMouseListener(new MouseAdapter() {     //Add moseRelease event in each Jpanel
                         @Override
                         public void mouseReleased(MouseEvent e) {
-                            System.out.println("Position :" + col +","+ row );
-							//if(othelloGame.ValidMove(row, col, gameBoard)){
-                           othelloGame.Move(row, col, gameBoard, true);
-							try{
-                            	Update(gameBoard);
-							} catch(IOException e3){}
-							//}
-                         // check vaild move here ! 
+                            System.out.println("Position :" + col + "," + row);
+                            othelloGame.Move(row, col, gameBoard, true);  // do move action 
+                            try {
+                                Update(gameBoard);   // update whole game board
+                            } catch (IOException e3) {
+                            }
+                            //}
+                            // check vaild move here ! 
                         }
                     });
                 }
 
             }
-
-			char[][] availableMov = othelloGame.AvailableMove(gameBoard, true);
+            char[][] availableMov = othelloGame.AvailableMove(gameBoard, true); // show the available move ,when the game start
             for (int i = 0; i < boardSizeY; i++) {
                 for (int j = 0; j < boardSizeX; j++) {
-                    if (availableMov[i][j] == 'O'){
-						if (othelloGame.GetPlayerTurn()){
-	                        squareBoard[i][j].setBackground(new Color(255, 0, 0));
-	                    } else {
-	                        squareBoard[i][j].setBackground(new Color(0, 0, 255));
-	                    }
+                    if (availableMov[i][j] == 'O') {
+                        if (othelloGame.GetPlayerTurn()) {
+                            squareBoard[i][j].setBackground(new Color(255, 0, 0));
+                        } else {
+                            squareBoard[i][j].setBackground(new Color(0, 0, 255));
+                        }
                     }
                 }
             }
-			System.out.println("White turn");
-		} else {
-			System.out.print("Connect4");
-		}
+            System.out.println("White turn");
+        } else {
+            System.out.print("Connect4");
+        }
 
     }
-    
-    public void Update(int[][] gameBoard) throws IOException{
-		for (int i = 0; i < boardSizeY; i++) {
-		    for (int j = 0; j < boardSizeX; j++) {
-			    //final JPanel square = new JPanel();
-				// squareBoard[i][j] = square;
-		        if (gameBoard[i][j] == black){
-		           //square.setBackground(new Color(0, 0, 0));
-		            squareBoard[i][j].removeAll();
-		            squareBoard[i][j].add(new JLabel(blackpiece));
-					squareBoard[i][j].updateUI();
-		               //System.out.println(gameBoard[i][j]);
-		        }
-		        else if (gameBoard[i][j] == white){
-					squareBoard[i][j].removeAll();
-					squareBoard[i][j].add(new JLabel(whitepiece));
-					squareBoard[i][j].updateUI();
-		        }
-			}
-		}
-			
-			char[][] availableMov = othelloGame.AvailableMove(gameBoard, true);
-            for (int i = 0; i < boardSizeY; i++) {
-                for (int j = 0; j < boardSizeX; j++) {
-					squareBoard[i][j].setBackground(new Color(170, 150, 100));
-                    if (availableMov[i][j] == 'O'){
-						if (othelloGame.GetPlayerTurn()){
-	                        squareBoard[i][j].setBackground(new Color(255, 0, 0));
-	                    } else {
-	                        squareBoard[i][j].setBackground(new Color(0, 0, 255));
-	                    } 
+
+    private void Update(int[][] gameBoard) throws IOException {
+        for (int i = 0; i < boardSizeY; i++) {
+            for (int j = 0; j < boardSizeX; j++) {
+                if (gameBoard[i][j] == black) {  //add the pieces into game board
+                    squareBoard[i][j].removeAll();
+                    squareBoard[i][j].add(new JLabel(blackpiece));
+                    squareBoard[i][j].updateUI();
+                    //System.out.println(gameBoard[i][j]);
+                } else if (gameBoard[i][j] == white) {
+                    squareBoard[i][j].removeAll();
+                    squareBoard[i][j].add(new JLabel(whitepiece));
+                    squareBoard[i][j].updateUI();
+                }
+            }
+        }
+
+        char[][] availableMov = othelloGame.AvailableMove(gameBoard, true);  // check the available move again when the piece is placed.
+        for (int i = 0; i < boardSizeY; i++) {
+            for (int j = 0; j < boardSizeX; j++) {
+                squareBoard[i][j].setBackground(new Color(170, 150, 100));
+                if (availableMov[i][j] == 'O') {
+                    if (othelloGame.GetPlayerTurn()) {
+                        squareBoard[i][j].setBackground(new Color(255, 0, 0));
+                    } else {
+                        squareBoard[i][j].setBackground(new Color(0, 0, 255));
                     }
                 }
-            } 
-			if(!othelloGame.winningCondition()){
-			if (othelloGame.GetPlayerTurn())System.out.println("Black turn"); else System.out.println("White turn");}
+            }
+        }
+        if (!othelloGame.winningCondition()) {   // check winning condition
+            if (othelloGame.GetPlayerTurn()) {
+                System.out.println("Black turn");
+            } else {
+                System.out.println("White turn");
+            }
+        }
 
     }
 
@@ -226,9 +223,10 @@ public class GameBoard extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-				try {
-					new GameBoard(true).setVisible(true);
-				} catch (IOException e2) {}
+                try {
+                    new GameBoard(true).setVisible(true);
+                } catch (IOException e2) {
+                }
             }
         });
     }
--- Game/Othello.java
+++ Game/Othello.java
@@ -1,18 +1,18 @@
-import java.awt.Color;
 
 //public class Othello extends Game {
-public class Othello{
+public class Othello {
+
     private int[][] gameBoard;
-    private int scoreWhite, scoreBlack, boardLength = 8, initialScore = 0;
-    private boolean playerTurn; //ture = black piece player, false = white piece player.
-    private int empty = 0, black = 1, white = 2, counter;
-	private int[] flipdata;
-	private char[][] AvailableMov;
+    private int scoreWhite, scoreBlack, initialScore = 0, counter;
+    private boolean playerTurn;
+    final private int empty = 0, black = 1, white = 2, boardLength = 8;
+    private int[] flipdata;
+    private char[][] AvailableMov;
     //  GameBoard gb = new GameBoard(true);
-    HumanPlayer P1 = new HumanPlayer("P1", false, black);
+    HumanPlayer P1 = new HumanPlayer("P1", false, black); //add the Player (should be do it in selection class)
     HumanPlayer P2 = new HumanPlayer("P2", true, white);
 
-    public void Othello(HumanPlayer P1, HumanPlayer P2) {
+    public void Othello(HumanPlayer P1, HumanPlayer P2) {   // get player
         if (P1.GetPlayerTurn() == true) {
             playerTurn = P1.GetPlayerTurn();
         } else {
@@ -20,32 +20,29 @@ public class Othello{
         }
 
     }
-    //add playerTurn as parameter, maybe 2d array board as output(set Available markers as value 3 in the array)?
 //
 //    public boolean SetPlayer(boolean playerTurn) {
 //        this.playerTurn = playerTurn;
 //    }
 
     public boolean GetPlayerTurn() {
-           return playerTurn = P1.GetPlayerTurn();
+        return playerTurn = P1.GetPlayerTurn();
     }
 
     public void SetPlayerTurn(boolean playerTurn) {
         this.playerTurn = playerTurn;
     }
 
-/*public String GetFirstPlayer() {
-	if (playerTurn == true){
-		return selection.getplayerName();
-	} else {
-		return selection.getCPUName();
-	}
-}*/
-
-    
+    /*public String GetFirstPlayer() {
+     if (playerTurn == true){
+     return selection.getplayerName();
+     } else {
+     return selection.getCPUName();
+     }
+     }*/
     //need calcWhiteScore and calcBlackScore from OthelloFB?
-    public int GetScoreWhite() {
-       scoreWhite = initialScore;
+    private int GetScoreWhite() {
+        scoreWhite = initialScore;
         for (int x = 0; x < boardLength; x++) {
             for (int y = 0; y < boardLength; y++) {
                 if (gameBoard[y][x] == white) {
@@ -56,7 +53,7 @@ public class Othello{
         return scoreWhite;
     }
 
-    public int GetScoreBlack() {
+    private int GetScoreBlack() {
         scoreBlack = initialScore;
         for (int x = 0; x < boardLength; x++) {
             for (int y = 0; y < boardLength; y++) {
@@ -68,12 +65,12 @@ public class Othello{
         return scoreBlack;
     }
 
-    public int[][] GetNewPiecePosition() {
+    public int[][] GetNewPiecePosition() {   // no idea why is there
         return null;
     }
 
-    public char[][] AvailableMove(int[][] gameBoard, boolean playerTurn) {
-
+    public char[][] AvailableMove(int[][] gameBoard, boolean playerTurn) {   // check available move and return the char array.     
+        // 'O' means available move.
         AvailableMov = new char[boardLength][boardLength];
         for (int i = 0; i < boardLength; i++) {
             for (int j = 0; j < boardLength; j++) {
@@ -89,7 +86,7 @@ public class Othello{
 
     }
 
-    public void CheckPassTurn(int[][] gameBoard) {
+    private void CheckPassTurn(int[][] gameBoard) {     // check the player if he has no any available move , pass the turn to opponent.
         boolean passTurn = true;
         for (int i = 0; i < boardLength; i++) {
             for (int j = 0; j < boardLength; j++) {
@@ -112,7 +109,7 @@ public class Othello{
         for (int i = 0; i < boardLength; i++) {         // After pass the turn to opponent , check the available move again 
             for (int j = 0; j < boardLength; j++) {
                 if (validMove(i, j, gameBoard, playerTurn)) {
-                    AvailableMov[i][j] = 'O';   //Location of Available Move 
+                    AvailableMov[i][j] = 'O';
                 } else {
                     AvailableMov[i][j] = 'X';
                 }
@@ -121,48 +118,44 @@ public class Othello{
         }
     }
 
-    public boolean ChangePieces(int[][] newPiecePosition) {
-        return true;
-    }
-
-    public void game(int[][] gameBoard, boolean playerTurn) {
+    public void game(int[][] gameBoard, boolean playerTurn) {   //??
 
     }
 
-    public int[][] Move(int y, int x, int[][] gameBoard, boolean playerTurn) {
-        if (validMove(y, x, gameBoard, playerTurn)) {
-            if (P1.GetPlayerTurn()) {
-		do {
-                    gameBoard = Flip(flipdata, gameBoard);
-		} while (validMove(y, x, gameBoard, playerTurn));
-		gameBoard[y][x] = P1.GetColor();
+    public int[][] Move(int y, int x, int[][] gameBoard, boolean playerTurn) {   //move action 
+        if (validMove(y, x, gameBoard, playerTurn)) {        // check whether the move is valid or not.
+            if (P1.GetPlayerTurn()) {            // check the player turn 
+                do {
+                    gameBoard = Flip(flipdata, gameBoard);             //do the flip action here 
+                } while (validMove(y, x, gameBoard, playerTurn));     // while it is valid move 
+                gameBoard[y][x] = P1.GetColor();         // add the piece into gameBoard
                 P1.SetPlayerTurn(false);
                 P2.SetPlayerTurn(true);
-				this.gameBoard = gameBoard;//for other method in this class
-				System.out.println("Black score:"+GetScoreBlack());
-				System.out.println("White score:"+GetScoreWhite());
+                this.gameBoard = gameBoard;  // get gameboard array from gameBoard.
+                System.out.println("Black score:" + GetScoreBlack());
+                System.out.println("White score:" + GetScoreWhite());
                 return gameBoard;
             } else {
-		do {
+                do {
                     gameBoard = Flip(flipdata, gameBoard);
-		} while(validMove(y, x, gameBoard, playerTurn));
-		gameBoard[y][x] = P2.GetColor();
+                } while (validMove(y, x, gameBoard, playerTurn));
+                gameBoard[y][x] = P2.GetColor();
                 P2.SetPlayerTurn(false);
                 P1.SetPlayerTurn(true);
-				this.gameBoard = gameBoard;
-				System.out.println("Black score:"+GetScoreBlack());
-				System.out.println("White score:"+GetScoreWhite());
+                this.gameBoard = gameBoard;
+                System.out.println("Black score:" + GetScoreBlack());
+                System.out.println("White score:" + GetScoreWhite());
                 return gameBoard;
             }
-		
-        }	
 
-	//if (GetPlayerTurn())System.out.println(P2.GetColor()+" turn"); else System.out.println(P1.GetColor()+" turn");
-		this.gameBoard = gameBoard;
+        }
+
+        //if (GetPlayerTurn())System.out.println(P2.GetColor()+" turn"); else System.out.println(P1.GetColor()+" turn");
+        this.gameBoard = gameBoard;
         return gameBoard;
     }
 
-    public boolean validMove(int y, int x, int[][] gameBoard, boolean playerturn) {
+    private boolean validMove(int y, int x, int[][] gameBoard, boolean playerturn) { // check valid move here (OTHELLO CORE)
 
         if (gameBoard[y][x] == 0) {
             int searchX, searchY;
@@ -173,7 +166,7 @@ public class Othello{
             } else {
                 playerColor = P2.GetColor();
             }
-	    counter = 1;
+            counter = 1;
 
             // Search each direction (total : 8 direction)
             for (int i = -1; i <= 1; i++) {
@@ -182,7 +175,8 @@ public class Othello{
                     searchY = y + i;
                     searchX = x + j;
                     if ((searchX >= boardLength || searchX < 0) || (searchY >= boardLength || searchY < 0)) {
-                        //System.out.println("Over bound! Y:" + searchY + " X: " + searchX + " i:" + i + " j: " + j);
+                        //System.out.println("Over bound! Y:" + searchY + " X: " + searchX + " i:" + i + " j: " + j);  
+                        //Debug message 
                         continue;
 
                     } else {
@@ -194,10 +188,11 @@ public class Othello{
                     if ((i == 0 && j == 0) || searchValue == empty || searchValue == playerColor) {
                         continue;
                     }
-					counter = 1;
+                    counter = 1;
                     //Searching along the direction
                     while (!found) {
                         //System.out.println("Looping..");
+                        //Debug message
                         searchX += j;
                         searchY += i;
 
@@ -211,36 +206,37 @@ public class Othello{
                         // if find the same color is along to direction 
                         if (searchValue == playerColor) {
                             found = true;
-							flipdata = new int[] {searchY,searchX,i,j,counter,playerColor};
+                            flipdata = new int[]{searchY, searchX, i, j, counter, playerColor};   // store the piece and ready to flip 
                             //System.out.println("found : true, Y:" + searchY + ", X:" + searchX + ", newp value:" + searchValue);
+                            //Debug message
                             return true;
                         } //If end of the direction is empty , then stop search this direction.
                         else if (searchValue == empty) {
                             found = true;
                             //System.out.println("empty : true, Y:" + searchY + ", X:" + searchX);
+                            //Debug message
                         }
-			counter++;
+                        counter++;
                     }
                 }
 
             }
-            //System.out.println("false");
+
             return false;
         }
-        //System.out.println("false");
         return false;
 
     }
 
-	public int[][] Flip(int[] flipdata, int[][] gameBoard) {
-		//System.out.println("FLIP "+flipdata[4]+" PIECE(S)");
-			for(int a = 0; a<flipdata[4]; a++){
-				flipdata[1] -= flipdata[3];
-				flipdata[0] -= flipdata[2];
-				gameBoard[flipdata[0]][flipdata[1]] = flipdata[5];
-			}
-		return gameBoard;
-	}
+    private int[][] Flip(int[] flipdata, int[][] gameBoard) {
+        //System.out.println("FLIP "+flipdata[4]+" PIECE(S)");
+        for (int a = 0; a < flipdata[4]; a++) {
+            flipdata[1] -= flipdata[3];
+            flipdata[0] -= flipdata[2];
+            gameBoard[flipdata[0]][flipdata[1]] = flipdata[5];
+        }
+        return gameBoard;
+    }
 
     //change to string as output
     public boolean winningCondition() {
@@ -252,7 +248,7 @@ public class Othello{
                 }
             }
         }
-		System.out.println("END");
+        System.out.println("END");
         return true;
 //        if (GetScoreBlack() == GetScoreWhite()) {
 //            return "draw";
