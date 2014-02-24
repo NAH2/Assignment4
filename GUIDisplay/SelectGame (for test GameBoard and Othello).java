a2de7a65e430059774aedf0d657cda32f49d56b0
modsz <supermodsz@gmail.com>
2014-02-24 20:58:37 +0000

    Added a selectGame class , in order to test othello game board & connect4 board

---
+++ GUIDisplay/SelectGame (for test GameBoard and Othello).java
@@ -0,0 +1,45 @@
+import javax.swing.*;
+import java.io.IOException;
+
+//public class SelectGame extends GUIDisplay {
+public class SelectGame {
+	private String[] game = {"Othello", "ConnectFour"};
+	private String chosenGame;
+	HumanPlayer P1 = new HumanPlayer("P1", false, new OthelloPiece("black"));
+	HumanPlayer P2 = new HumanPlayer("P2", true, new OthelloPiece("white"));
+
+	private boolean SetGame(String chosenGame) {
+		this.chosenGame = chosenGame;
+		return true;
+	}
+
+	public String GetGame() {
+		return chosenGame;
+	}
+
+	
+	public boolean draw() {	
+		chosenGame = JOptionPane.showInputDialog(null,"GAME", "Choose a game to play", JOptionPane.INFORMATION_MESSAGE, null, game, game[0])+""; //return an object so add empty string
+		try {
+			if(chosenGame.equals("Othello")){
+				GameBoard GB = new GameBoard(chosenGame, P1, P2);
+				GB.setVisible(true);
+				//Selection SE = new Selection(chosenGame);
+			} else {
+				GameBoard GB = new GameBoard(chosenGame, P1, P2);
+                                GB.setVisible(true);
+				//Selection SE = new Selection(chosenGame);
+			}
+		} catch (IOException e) {}
+
+		return true;
+	}
+
+	//public boolean clear() {
+	//}
+
+	public static void main(String[] args) {
+		SelectGame a = new SelectGame();
+		a.draw();
+	}
+}
