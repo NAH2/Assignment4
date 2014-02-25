19f572d3348ea4eb0082a8e943c3fdfa9254666a
MathewLloyd <711293@swansea.ac.uk>
2014-02-25 22:17:43 +0000

    Added commenting
    Changed Player to be abstract, commenting is done to doxygen rules.

--- Player/HumanPlayer.java
+++ Player/HumanPlayer.java
@@ -1,47 +1,94 @@
-
 /**
- *
- * @author Matt
- * */
+ * \file -Player.java 
+ * \author -Mathew Lloyd 711293 
+ * \date -20th Feb 14
+ * 
+ * \see Player.java
+ * 
+ * \brief HumanPlayer, used to store data about the player,
+ * extends Player.java
+ * 
+ * This class extends from the Player class. It inherits the accessor methods,
+ * implements the methods in it's own way that differes to other player types.
+ */
 
 public class HumanPlayer extends Player{
 
-    private String playerName;
-    private boolean playerTurn;
-    private GamePiece piece;
-    
-    private HumanPlayer(){}
-    
-    public HumanPlayer(String playName, boolean playerTurn, GamePiece gamePiece) {
+   /**
+    * This is the constructor for the player
+    * calls to set the player name, turn and piece colour
+    * 
+    * \param playName a String that contains the players name
+    * \param playerTurn initializes this to true or false to set which player goes first
+    * \param gamePiece sets the players piece colour
+    * 
+    */
+    public HumanPlayer(String playName, boolean playerTurn, String gamePiece) {
         SetPlayerName(playName);
         SetPlayerTurn(playerTurn);
         SetPieceColor(gamePiece);
     }
 
-    public String GetPlayerName() {
-        return playerName;
-    }
 
-    public void SetPlayerName(String settingPlayerName) {
-        playerName = settingPlayerName;
+    /**
+     * An accessor method to retrieve the players 
+     *  name from the Player class.
+     *  \return m_playerName returns the player name
+     *  retrieves from Player class
+     */
+    public String GetPlayerName(){
+    	return m_playerName;
     }
-
+    
+    /** An accessor method to retrieve the players colour piece
+     * \return m_pieceColor the players colour.
+     * retrieves from Player class
+     */
+    public String GetPiece() {
+        return m_pieceColor;
+    }
+    
+    /** 
+     * An accessor method to retrieve whether it's the
+     *  players turn or not
+     *  \return m_playerTurn (true = players turn, false = not players turn)
+     * retrieves from Player class
+     */
     public boolean GetPlayerTurn() {
-        return playerTurn;
+    	return m_playerTurn;
+    }
+    
+    
+    
+
+    /**
+     * An accessor method to set the players name.
+     * \param settingPlayerName parameter comes from the constructor,
+     * sets the players name in the Player class.
+     */
+    public void SetPlayerName(String settingPlayerName) {
+        m_playerName = settingPlayerName;
     }
 
+    /**
+     * An accessor method to set the players turn.
+     * \param settingPlayerTurn parameter sets the whether it's
+     * the players turn in the Player class.
+     */
     public void SetPlayerTurn(boolean settingPlayerTurn) {
-        playerTurn = settingPlayerTurn;
+        m_playerTurn = settingPlayerTurn;
     }
 
-    public GamePiece GetPiece() {
-        return piece;
+    /**
+     * An accessor method to set the players game piece colour.
+     * \param gamePiece parameter comes from the constructor
+     * sets the players piece colour in the Player class.
+     */
+    public void SetPieceColor(String gamePiece) {
+        m_pieceColor = gamePiece;
     }
-
-    public void SetPieceColor(GamePiece gamePiece) {
-        piece = gamePiece;
-    }
-
+    
+    
 
 
     
@@ -49,6 +96,10 @@ public class HumanPlayer extends Player{
      * For testing purposes have a main method.
      */
     public static void main(String args[]){
-    	HumanPlayer humanPlayer = new HumanPlayer();
+    	HumanPlayer humanPlayer = new HumanPlayer("Player", true,"yellow");
+    	System.out.println(humanPlayer.GetPlayerName());
+    	System.out.println(humanPlayer.GetPiece());
+    	System.out.println(humanPlayer.GetPlayerTurn());
+    	
     }
 }
--- Player/Player.java
+++ Player/Player.java
@@ -1,35 +1,67 @@
-package Player;
+/**
+ * \file -Player.java 
+ * \author -Mathew Lloyd 711293 
+ * \date -20th Feb 14
+ * 
+ * \see HumanPlayer.java
+ * \see http://docs.oracle.com/javase/tutorial/java/javaOO/accesscontrol.html
+ * 
+ * \brief Parent class of methods for HumanPlayer.java
+ * 
+ * This abstract class is the parent object for the different types of players,
+ * it contains accessor methods for player data and abstract method declarations
+ */
 
 public abstract class Player {
+
+	/** \return m_pieceColor players piece colour */
+	public String GetPiece() {
+		return m_pieceColor;
+	}
 	
-	private String playerName;
-    private boolean playerTurn;
-    private GamePiece pieceColor;
+	/** \return m_playerName the players name */
+	public String GetPlayerName() {
+		return m_playerName;
+	}
+	/** 
+	 * Accessor method to get the players turn.
+	 * 		\return m_playerTurn true if it is players turn
+	 * 		\return m_playerTurn false if it is not the players turn
+	 */
+	public boolean GetPlayerTurn() {
+		return m_playerTurn;
+	}
 
-    public String GetPlayerName() {
-    	return playerName;
-    	}
-    
-    public void SetPlayerName(String settingPlayerName) {}
+	/** Abstract accessor method to set the piece colour for the player.
+	 * \param gamePiece a String containing the colour of the gamePiece
+	 */
+	public abstract void SetPieceColor(String gamePiece);
+	
+	/** Abstract accessor method to set the players name.
+	 *  \param settingPlayerName settingPlayerName = the players name that gets set.
+	 */
+	public abstract void SetPlayerName(String settingPlayerName);
 
-    public boolean GetPlayerTurn() {
-    	return playerTurn;
-    }
-    
-    public void SetPlayerTurn(boolean settingPlayerTurn) {}
+	/** 
+	 * Abstract accessor method to set the players turn.
+	 * \param settingPlayerTurnsetting whether it's the players turn or not.
+	 * 		true = players turn
+	 * 		false = not the players turn
+	 */
+	public abstract void SetPlayerTurn(boolean settingPlayerTurn);
 
-    public GamePiece GetColor() {
-    	return pieceColor;
-    }
+	
 
-    public void SetPieceColor(GamePiece colorPiece) {}
-
-
-
-    
-    /**
-     * For testing purposes have a main method.
-     */
-    public static void main(String args[]){
-    }
+	/**
+	 * For testing purposes have a main method.
+	 */
+	public static void main(String args[]) {
+	}
+	
+	/** The players set name */
+	protected String m_playerName;
+	/** Boolean of whether it is players turn*/
+	protected boolean m_playerTurn;
+	/** The players piece colour */
+	protected String m_pieceColor;
 }
