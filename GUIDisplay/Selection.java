dc9c916a2123fe8c9639d57dddcf3e8d62831cad
tomlet <letheby1@live.co.uk>
2014-02-26 13:48:31 +0000

    Selection class
    Commented with code conventions

--- GUIDisplay/Selection.java
+++ GUIDisplay/Selection.java
@@ -4,7 +4,8 @@
  * \@date -25th Feb 14
  * 
  * \see SelectGame.java
- * \see something
+ * \see GameController.java
+ * \see HumanPlayer.java
  * 
  * \brief Selection class, used to allow the user to select opponent names, opponent types 
  * 
@@ -17,10 +18,12 @@ import java.awt.*;
 
 public class Selection implements ActionListener {
 
-    private String currentGameType;
+	private String currentGameType;
     private String chosenGame;
     private int FRAME_WIDTH = 250;
     private int FRAME_HEIGHT = 250;
+    private int FIELD_SIZE = 10;
+    private int FIELD_SIZE2 = 10;
     private JFrame display;
     private JPanel btnPanel;
     private JPanel selectPanel;
@@ -35,7 +38,7 @@ public class Selection implements ActionListener {
     private JComboBox selectOpponent;
 
     /**
-     * constructor of Selection, receives the type of game, ready for later.
+     * Constructor of Selection, receives the type of game, ready for later.
      * @param chosenGame a String for the chosen game.
      */
     public Selection(String chosenGame) {
@@ -54,119 +57,166 @@ public class Selection implements ActionListener {
      * Accessor method to get the current game type
      * @return currentGameType a String of what the current game type is
      */    
-    public String getGameType() {
+    public String GetGameType() {
         return currentGameType;
     }
 
+    /**
+     * Accessor method to set the player ones name
+     * @param playerName a String for the players name
+     */
     private void setPlayerNameOne(String playerName) {
         p1Text = playerName;
     }
 
+    /**
+     * Accessor method to set the player twos name
+     * @param playerName a String for the players name
+     */
     private void setPlayerNameTwo(String playerName){
-    	p2Text=playerName;
+    	p2Text = playerName;
     }
     
    
-    public void draw() {
-        display = new JFrame();
-
+    /**
+     * The method that sets up and draws the JFrame and its corresponding elements
+     */
+    public void Draw() {
+        ///sets up JFrame
+    	display = new JFrame();
         display.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         display.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         display.setTitle("Select Players");
-
+        
+        /// Sets up the JPanels within the JFrame and set their flow layout to center
         JPanel btnPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JPanel selectPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        
+        /// Sets up a JComboBox and adds it to the selectPanel JPanel, also adds the action listener
         selectOpponent = new JComboBox(opponentList);
         selectOpponent.setSelectedIndex(0);
         selectOpponent.addActionListener(this);
         selectPanel.add(selectOpponent);
-        JLabel oppoLevel = new JLabel("Select Opponent Level");
-        playerOne = new JTextField("player one name:", 20);
-        playerTwo = new JTextField("player two name:", 20);
+        
+        ///sets up the players one and two JTextFields and adds the action listener + mouse listener to both
+        playerOne = new JTextField("player one name:", FIELD_SIZE);
+        playerTwo = new JTextField("player two name:", FIELD_SIZE2);
         playerOne.addActionListener(this);
         playerOne.addMouseListener(new MouseAdapter(){
         	public void mouseClicked(MouseEvent e){
         		playerOne.setText("");
         	}
-        });
-        
+        }
+        );        
         playerTwo.addActionListener(this);
         playerTwo.addMouseListener(new MouseAdapter(){
         	public void mouseClicked(MouseEvent e){
         		playerTwo.setText("");
         	}
-        });
+        }
+        );        
+        ///Adds the JButtons "Cancel" and "play" and adds the action listeners to both
         btn_cancel = new JButton("Cancel");
         btn_play = new JButton("Play");
         btn_cancel.addActionListener(this);
         btn_play.addActionListener(this);
+        
+        ///Adds the JButtons "cancel" and "play" to the JPanel btnPanel.
         btnPanel.add(btn_cancel);
         btnPanel.add(btn_play);
+        
+        ///Adds the JTextFields for player one and player two to the JPanel selectPanel.
         selectPanel.add(playerOne);
         selectPanel.add(playerTwo);
-
+        
+        ///adds the btnPanel to the JFrame and aligns it to the lower part of the frame
         display.add(btnPanel, BorderLayout.SOUTH);
+        ///adds the selectPanel to the JFrame and sets the JFrame to be visible
         display.add(selectPanel);
         display.setVisible(true);
-
-    }
+    	}
     
-    
-public static void main(String args[]) {
+    ///Will be used for testing
+    	public static void main(String args[]) {
 		
-		Selection selectClass = new Selection(opponentOthello); //test case
-		selectClass.draw();
+		Selection selectClass = new Selection(opponentOthello); //TEST CASE
+		selectClass.Draw();
 		}
-
+    	
 @Override
-public void actionPerformed(ActionEvent e) {
-	if (e.getSource()== selectOpponent) {
+		/**
+		* Sets up the action listeners for the objects needed for Listeners
+		*/	
+		public void actionPerformed(ActionEvent e) {
+		///This if statement implements a action listener for the JComboBox
+		if (e.getSource()== selectOpponent) {
+		///Setting opponent type to Human or AI 
 		opponentType = (String) selectOpponent.getSelectedItem();
-		System.out.println(opponentType);
-	}
-	if (e.getSource()== playerOne) {
-		setPlayerNameOne(playerOne.getText());
-		System.out.println(p1Text);
-	}
-	if (e.getSource()== playerTwo) {
-		setPlayerNameTwo(playerTwo.getText());
-		System.out.println(p2Text);
-	}
-	if (e.getSource()== btn_cancel) {
-		display.dispose();
-	}
+		}
 	
-	if (e.getSource()== btn_play) {
-		GameController gameCont = new GameController();
+		///This if statement implements a action listener for the first JTextField for player one
+		if (e.getSource()== playerOne) {
+		///setting the Player one name to what is typed into the text field
+		setPlayerNameOne(playerOne.getText());
+		}
+	
+		///This if statement implements a action listener for the second JTextField for player two
+		if (e.getSource()== playerTwo) {
+		//////setting the Player two name to what is typed into the text field
+		setPlayerNameTwo(playerTwo.getText());
+		}
+	
+		///this if statement implements a action listener for the JButton btn_cancel
+		if (e.getSource()== btn_cancel) {
+		///Closes the JFrame completely when button is performed
+		display.dispose();
+		}
+	
+		///The if statement implements a action listener for the JButton btn_play
+		if (e.getSource()== btn_play) {
+		
+		///this if statement checks if the chosenGame is Othello
 		if (chosenGame.equalsIgnoreCase(opponentOthello)) {
-			//HumanPlayer HuPlay = new HumanPlayer(p1Text,"black");
+			///then the first human player is set to the piece colour black 
+			HumanPlayer HuPlay = new HumanPlayer(p1Text,"black");
+			
+			///this if statement checks if the opponentType is a Human player
 			if (opponentType.equals("Human")){
-				//HumanPlayer HuPlayOpponent = new HumanPlayer(p2Text,"white");
+				///then the second human player is set to the piece colour white
+				HumanPlayer HuPlayOpponent = new HumanPlayer(p2Text,"white");
+				///closes the JFrame display entirely
 				display.dispose();
+				///creates a new gameController object
 				GameController gameCont = new GameController(opponentOthello, huPlay, huPlayOpponent);
 			}
+			
 			else {
+				///displays a error message dialog box, explaining that AI can't be played in current version
 				JOptionPane.showMessageDialog(null, "AI not available in this version.", "Error", JOptionPane.ERROR_MESSAGE);
 			}
 			}
-		else if (chosenGame.equalsIgnoreCase(opponentConnectFour)) {
-			//HumanPlayer HuPlay = new HumanPlayer(p1Text,"red");
-			if (opponentType.equals("Human")){
-				//HumanPlayer HuPlayOpponent = new HumanPlayer(p2Text,"yellow");
+		
+			///this else if statement checks whether the chosen game is Connect Four
+			else if (chosenGame.equalsIgnoreCase(opponentConnectFour)) {
+				///then the first human player is set to the piece colour red
+				HumanPlayer HuPlay = new HumanPlayer(p1Text,"red");
+			
+				///this if statement checks if the opponentType is a human player
+				if (opponentType.equals("Human")){
+				///then the second human player is set to the piece colour yellow
+				HumanPlayer HuPlayOpponent = new HumanPlayer(p2Text,"yellow");
+				///closes the JFrame display entirely
 				display.dispose();
+				///creates a new gameController object
 				GameController gameCont = new GameController(opponentConnectFour, huPlay, huPlayOpponent);
-			}
-			else {
+				}
+				else {
+				///displays a error message dialog box, explaining that AI can't be played in current version
 				JOptionPane.showMessageDialog(null, "AI not available in this version.", "Error", JOptionPane.ERROR_MESSAGE);
+				}
 			}
-			}
-	}
-	
-	
-	
+		}	
+	}				
 }
 		
-		
-}
-		//jframe for options to be selected
 
