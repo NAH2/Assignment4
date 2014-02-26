d25ca9a52e76fe9bf66b6a339aabeb9d246f5afd
jakeplumley <jakeplumley@hotmail.co.uk>
2014-02-26 11:36:15 +0000

    updated

--- Game/Connect4.java
+++ Game/Connect4.java
@@ -2,16 +2,17 @@
 
 public class Connect4 {   //upper Case 
 
-    //  private int[][] gameBoard;
+    private int[][] gameBoard;
     private int scoreYellow, scoreRed, initialScore = 0, counter, counterRed, counterYellow;
     private boolean playerTurn;
-    // final private int empty = 0, Red = 1, Yellow = 2, boardX = 10, boardY=7;
+    final private int empty = 0, Red = 1, Yellow = 2;
     final private int boardX = 10, boardY = 7;
     private char[][] AvailableMov;
-    // HumanPlayer P1 = new HumanPlayer("P1", false, Red); //add the Player (should do it in selection class)
+     //HumanPlayer P1 = new HumanPlayer("P1", false, Red); //add the Player (should do it in selection class)
     //HumanPlayer P2 = new HumanPlayer("P2", true, Yellow);
     private HumanPlayer P1;
     private HumanPlayer P2;
+    boolean winning_move;
 
     public Connect4(HumanPlayer P1, HumanPlayer P2) {
         this.P1 = P1;
@@ -90,19 +91,13 @@ public class Connect4 {   //upper Case
     public void game(int[][] gameBoard, boolean playerTurn) {   //??
 
     }
+    
+    
 
     public GamePiece[][] Move(int y, int x, int[][] gameBoard, boolean playerTurn) {   //move action
         if (validMove(y, x, gameBoard, playerTurn)) {        // check whether the move is valid or not.
             if (P1.GetPlayerTurn()) {            // check the player turn
-                do {
-                    gameBoard = Flip(flipdata, gameBoard);             //do the flip action here
-                } while (validMove(y, x, gameBoard, playerTurn));     // while it is valid move
-                gameBoard[y][x] = P1.GetColor();         // add the piece into gameBoard
-                P1.SetPlayerTurn(false);
-                P2.SetPlayerTurn(true);
-                this.gameBoard = gameBoard;  // get gameboard array from gameBoard.
-                System.out.println("Red score:" + GetScoreRed());
-                System.out.println("Yellow score:" + GetScoreYellow());
+               
                 if (P2.GetColor() == Red) {
                     System.out.println("Player 2(Red) Turn");
                 } else {
@@ -134,87 +129,20 @@ public class Connect4 {   //upper Case
         return gameBoard;
     }
 
-    //  (connect4 CORE)    edited to basically do the same thing but to count the number of red/yellow counters in a sequence.
-    private boolean playerScoreCounter(int y, int x, GamePiece[][] gameBoard, boolean playerturn) {
-
-        // if (gameBoard[y][x] == 0) {
-        if (gameBoard[y][x] == null) {
-            int searchX, searchY;
-            //int searchValue, playerColor, Y;
-            GamePiece searchValue, playerColor;
-
-            if (P1.GetPlayerTurn() == true) {
-                playerColor = P1.GetPiece();
-            } else {
-                playerColor = P2.GetPiece();
-            }
-            counter = 1;
-
-            // Search each direction (total = 5 direction)
-            for (int i = 0; i <= 1; i++) {
-                for (int j = -1; j <= 1; j++) {
-                    boolean found = false;
-                    searchY = y + i;
-                    searchX = x + j;
-
-                    //states that the search cannot be outside the parameter of the board!!
-                    if ((searchX >= boardX || searchX < 0) || (searchY >= boardY || searchY < 0)) {
-                        //System.out.println("Over bound! Y:" + searchY + " X: " + searchX + " i:" + i + " j: " + j);
-                        //Debug message
-                        continue;
-
-                    } else {
-                        searchValue = gameBoard[searchY][searchX];
-                    }
-                    // skip the search if i and j is 0.
-                    // skip the search if one of the direction is empty
-                    // skip the search if one of the direction is same color piece (REMOVED)
-                    if ((i == 0 && j == 0) || searchValue == null) {
-                        continue;
-                    }
-
-                    //Searching along the direction
-                    while (!found) {
-                        //System.out.println("Looping..");
-                        //Debug message
-                        searchX += j;
-                        searchY += i;
-
-                        //prevent out of bound, if x over 10 and y over 7 , exit the while loop and stop searching this direction
-                        if ((searchX >= boardX || searchX < 0) || (searchY >= boardY || searchY < 0)) {
-                            found = true;
-                            
-                        } else {
-                            searchValue = gameBoard[searchY][searchX];
-                        }
-
-                        // if find the same color is along to direction
-                        if (searchValue == playerColor) {
-                            found = true;
-                            counterRed++;
-                            return true;
-                            //  continue;  it should be keep going , rather than skip this direction
-
-                        } //                        if (searchValue == 2) {
-                        //                            found = true;
-                        //                            counterYellow++;
-                        //                            return true;
-                        //                            continue;
-                        // } If end of the direction is empty , then stop search this direction.
-                        else if (searchValue == null) {
-                            found = true;
-                            //System.out.println("empty : true, Y:" + searchY + ", X:" + searchX);
-                            //Debug message
-                        }
-                    }
+    public boolean horizontal_win (boolean winning_move) {
+        for (int x = 0; x < boardX; x++) {
+            for (int y = 0; y < boardY; y++) {
+                if(gameBoard[y][x] = P1.GetColor()){
+                    
                 }
-
             }
 
-            return false;
-        }
-        return false;
-
+    public boolean vertical_win (boolean winning_move) {
+        for (int y = 0; y < boardy; y++) {
+            for (int x = 0; x < boardX; x++) {
+                if(gameBoard[y][x] = P1.GetColor()){
+                
+                }
     }
 
     //checks if any player can make a move
