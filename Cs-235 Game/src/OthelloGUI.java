7918f263b9d0977d5b8b81524a4c9d1ed63ff028
modsz <supermodsz@gmail.com>
2014-02-26 23:52:55 +0000

    Changed available move color.

--- Cs-235 Game/src/OthelloGUI.java
+++ Cs-235 Game/src/OthelloGUI.java
@@ -1,28 +1,33 @@
-
 import java.awt.Color;
 import javax.swing.BorderFactory;
 import boardGame.*;
 
 public class OthelloGUI extends GUI {
 
-    public OthelloGUI(BoardGame b, GameController g) {
-        super(b, g);
-        setPassButton();
-    }
-    
-    public boolean setPanelColour() {
-        char[][] availableMoves = ((Othello) getBoard()).availableMove(getGame().getCurrent());
-        Color defCol = new Color(170,150,100);
-        for (int y = 0; y < getBoard().getHeight(); ++y) {
-            for (int x = 0; x < getBoard().getWidth(); ++x) {
-                m_panels[x][y].setBorder(BorderFactory.createLineBorder(Color.black));
-                if (availableMoves[x][y] == 'O') {
-                    m_panels[x][y].setBackground(Color.cyan);
-                } else {
-                    m_panels[x][y].setBackground(defCol);
-                }
-            }
-        }
-        return true;
-    }
+	public OthelloGUI(BoardGame b, GameController g) {
+		super(b, g);
+		setPassButton();
+	}
+
+	public boolean setPanelColour() {
+		char[][] availableMoves = ((Othello) getBoard())
+				.availableMove(getGame().getCurrent());
+		Color defCol = new Color(170, 150, 100);
+		for (int y = 0; y < getBoard().getHeight(); ++y) {
+			for (int x = 0; x < getBoard().getWidth(); ++x) {
+				m_panels[x][y].setBorder(BorderFactory
+						.createLineBorder(Color.black));
+				if (availableMoves[x][y] == 'O') {
+					if ((getGame().getCurrent() == "black"))
+						m_panels[x][y].setBackground(Color.blue);
+					else
+						m_panels[x][y].setBackground(Color.red);
+
+				} else {
+					m_panels[x][y].setBackground(defCol);
+				}
+			}
+		}
+		return true;
+	}
 }
