96bb35d72fa2cb994353ee84072e8c25c33da7b4
modsz <supermodsz@gmail.com>
2014-02-27 18:46:16 +0000

    Update OthelloGUITest.java

--- OthelloGUITest.java
+++ OthelloGUITest.java
@@ -5,22 +5,20 @@ import org.junit.Test;
 import Player.HumanPlayer;
 import boardGame.Othello;
 
-
 public class OthelloGUITest {
-	 HumanPlayer p1 = new HumanPlayer("Jim", "black");
-		HumanPlayer p2 = new HumanPlayer("Bob", "white");
-	   Othello othello = new Othello();
-   	GameController game = new GameController("othello", p1, p2);
-   	OthelloGUI OthelloGUI = new OthelloGUI(othello, game);
-		 
-	 
+	HumanPlayer p1 = new HumanPlayer("Jim", "black");
+	HumanPlayer p2 = new HumanPlayer("Bob", "white");
+	Othello othello = new Othello();
+	GameController game = new GameController("othello", p1, p2);
+	OthelloGUI OthelloGUI = new OthelloGUI(othello, game);
+
 	@Test
 	public void testSetOthelloInfo() {
 		assertTrue(OthelloGUI.SetOthelloInfo());
 	}
 
 	@Test
-	public void testSetPanelColour(){
+	public void testSetPanelColour() {
 		assertTrue(OthelloGUI.SetPanelColour());
 	}
-} 
+}
