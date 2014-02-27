8f9bf7019f5061403caab3b56a58b9c2f1353907
rubbish9529 <rubbish9529@yahoo.com.hk>
2014-02-27 03:10:49 +0000

    Update OthelloGUI.java

--- Cs-235 Game/src/OthelloGUI.java
+++ Cs-235 Game/src/OthelloGUI.java
@@ -1,15 +1,55 @@
 import java.awt.Color;
 import javax.swing.BorderFactory;
+import javax.swing.Icon;
+
+import piece.OthelloPiece;
 import boardGame.*;
 
 public class OthelloGUI extends GUI {
 
 	public OthelloGUI(BoardGame b, GameController g) {
 		super(b, g);
-		setPassButton();
 		setOthelloInfo();
 	}
 
+	public boolean setOthelloInfo(){
+		playerOneColor.setText(m_game.getPlayerName("black")+":");
+		playerOneColor.setFont(f);
+		playerOneColor.setVisible(true);
+		OthelloPiece black = new OthelloPiece("black");
+		playerOneIcon.setIcon(black.getIcon());
+		playerOneIcon.setVisible(true);
+		playerTwoColor.setText(m_game.getPlayerName("white")+":");
+		playerTwoColor.setFont(f);
+		playerTwoColor.setVisible(true);
+		OthelloPiece white = new OthelloPiece("white");
+		playerTwoIcon.setIcon(white.getIcon());
+		playerTwoIcon.setVisible(true);
+		playerTurnIcon.setIcon(new OthelloPiece(m_game.getCurrent()).getIcon());
+		playerTurnIcon.setVisible(true);
+		playerTurnLabel.setText("'s TURN");
+		playerTurnLabel.setFont(f);
+		playerTurnLabel.setVisible(true);
+		blackIcon.setIcon(black.getIcon());
+		blackIcon.setVisible(true);
+		blackPieces.setText(((Othello)(m_board)).GetBlackScore()+"");
+		blackPieces.setFont(f);
+		blackPieces.setVisible(true);
+		whiteIcon.setIcon(white.getIcon());
+		whiteIcon.setVisible(true);
+		whitePieces.setText(((Othello)(m_board)).GetWhiteScore()+"");
+		whitePieces.setFont(f);
+		whitePieces.setVisible(true);
+		m_passMove.setVisible(true);
+		m_frame.pack();
+		return true;
+	}
+	
+	public boolean updatePlayerTurnIcon(Icon picon){
+		playerTurnIcon.setIcon(picon);
+		return true;
+	}
+	
 	public boolean setPanelColour() {
 		char[][] availableMoves = ((Othello) getBoard())
 				.AvailableMove(getGame().getCurrent());
