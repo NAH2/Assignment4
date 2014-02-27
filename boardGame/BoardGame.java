2462c32d8577896b1a6118eb48421778f5fcf432
rubbish9529 <rubbish9529@yahoo.com.hk>
2014-02-27 15:02:53 +0000

    Update BoardGame.java

--- boardGame/BoardGame.java
+++ boardGame/BoardGame.java
@@ -1,23 +1,42 @@
-package boardGame;
+/**
+ * \file -BoardGame.java 
+ * \author -
+ * \date -20nd Feb 14
+ * 
+ * \brief BoardGame is an abstract class 
+ * \see ConnectFour.java
+ * \see Othello.java
+ * 
+ * BoardGame is the superclass of both Othello and Connect Four class
+ */
+ package boardGame;
 
 import piece.*;
 
 public abstract class BoardGame {
-    public BoardGame(int x, int y) {
-	   SetWidth(x);
-	   SetHeight(y);
-	   board = new GamePiece[m_width][m_height];
-	}
-
+	
+		 /**
+     * Set the board height.
+     * \return boolean  return true if the action completes..
+     */
 	public boolean SetHeight(int y){
 		m_height= y;
 		return true;
 	}
 
+	 /**
+     * Get the board height.
+     * \return int  return the board height.
+     */
 	public int GetHeight() {
 	    return m_height;
 	}
 
+	 /**
+     * Get the board height.
+     * \param x and y, the position of the GamePiece.
+     * \return GamePiece  return the GamePiece object of relative position.
+     */
 	public GamePiece GetPiece(int x, int y) {
 	    if  (board[x][y] != null) {
 	        return board[x][y];
@@ -26,8 +45,17 @@ public abstract class BoardGame {
 	    }
 	}
 
+		 /**
+     * Abstract method
+     * \param x and y, the position of the GamePiece, the colour of the piece.
+     * \return boolean
+     */
 	public abstract boolean SetPiece(int x, int y, String col);
 
+	 /**
+     * Get the total number of pieces on the game board.
+     * \return int  return the number of pieces on the game board.
+     */
 	public int GetTotalPieces() {
 	    int count = 0;
 	    for (int y = 0; y < m_height; ++y) {
@@ -40,30 +68,74 @@ public abstract class BoardGame {
 	    return count;
 	}
 
+		 /**
+     * Set the board width.
+     * \return boolean  return true if the action completes..
+     */
 	public boolean SetWidth(int x){
 		m_width = x;
 		return true;
 	}
 
+	 /**
+     * Get the board width.
+     * \return int  return the board width.
+     */
 	public int GetWidth() {
 	    return m_width;
 	}
 
+		/**
+     * Set the colour of pieces meet the winning condition to winningColour.
+     * \param the winning colour.
+     * \return boolean  return true if the action completes..
+     */
 	public boolean SetWinningColour(String winner) {
 	    m_winningColour = winner;
 	    return true;
 	}
 
+		 /**
+     * Get the winning colour.
+     * \return String  return the colour.
+     */
 	public String GetWinningColour() {
 	    return m_winningColour;
 	}
-
+		 
+		 /**
+     * Abstract method.
+     * \return boolean
+     */
 	public abstract boolean SetWinner();
 
+		 /**
+     * Constructor of BoardGame, create a game board that stores the game pieces.
+     * \param width and the height of the game board.
+     */
+	public BoardGame(int x, int y) {
+	   SetWidth(x);
+	   SetHeight(y);
+	   board = new GamePiece[m_width][m_height];
+	}
+	
+			 /**
+     * Abstract method.
+     * \param X,y position and the colour of the piece.
+     * \return boolean
+     */
 	public abstract boolean Move(int x, int y, String col);
 
+		 /**
+     * Abstract method.
+     * \return boolean
+     */
 	public abstract boolean WinningCondition();
 
+			 /**
+     * This method is used for testing
+     * \return String  gameBoard consists of texts.
+     */
 	@Override
 	public String toString() {
 	    String boardStateString = "";
