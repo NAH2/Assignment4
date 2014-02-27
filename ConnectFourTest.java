693fad7cced8c7a705e30200db0638c62355d172
rubbish9529 <rubbish9529@yahoo.com.hk>
2014-02-27 19:14:45 +0000

    Update ConnectFourTest.java

--- ConnectFourTest.java
+++ ConnectFourTest.java
@@ -1,6 +1,7 @@
 
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
 import Player.HumanPlayer;
@@ -18,15 +19,43 @@ public class ConnectFourTest {
 	 */
 	@Test
 	public void testSetPiece() {
+		/**Valid Test Cases*/
 		assertTrue(c4.SetPiece(9,6,"red"));
 		assertTrue(c4.SetPiece(0,0,"yellow"));
+		
+		/**Invalid Test Cases*/
 		assertFalse(c4.SetPiece(9, 7, "red"));
 		assertFalse(c4.SetPiece(10, 8, "yellow"));
 		assertFalse(c4.SetPiece(-1, -1, "yellow"));
 	}
 	
 	@Test
+	public void testGetWidth() {
+		/**Valid Test Cases*/
+		assertEquals(10, c4.GetWidth());
+
+		/**Invalid Test Cases*/
+		assertThat(0, not(c4.GetWidth()));
+		assertThat(-5, not(c4.GetWidth()));
+		assertThat(9, not(c4.GetWidth()));
+		assertThat(999, not(c4.GetWidth()));
+	}
+	
+	@Test
+	public void testGetHeight() {
+		/**Valid Test Cases*/
+		assertEquals(7, c4.GetHeight());
+
+		/**Invalid Test Cases*/
+		assertThat(0, not(c4.GetHeight()));
+		assertThat(-2, not(c4.GetHeight()));
+		assertThat(9, not(c4.GetHeight()));
+		assertThat(999, not(c4.GetHeight()));
+	}
+	
+	@Test
 	public void testMove() {
+		/**Valid Test Cases*/
 		assertTrue(c4.Move(9,6,"red"));
 		assertTrue(c4.Move(9,1,"yellow"));
 		assertTrue(c4.Move(9,6,"red"));
@@ -34,12 +63,17 @@ public class ConnectFourTest {
 		assertTrue(c4.Move(9,2,"red"));
 		assertTrue(c4.Move(9,3,"yellow"));
 		assertTrue(c4.Move(9,4,"red"));
+		
+		/**Invalid Test Cases*/
 		assertFalse(c4.Move(9,7,"red"));
 	}
 	
 	@Test
 	public void testWinningCondition() {
+		/**Invalid Test Cases*/
 		assertFalse(c4.WinningCondition());
+		
+		/**Valid Test Cases*/
 		c4.Move(9,6,"red");
 		c4.Move(9,6,"red");
 		c4.Move(9,6,"red");
@@ -49,12 +83,41 @@ public class ConnectFourTest {
 	
 	@Test
 	public void testSetWinner(){
+		/**Invalid Test Cases*/
 		assertFalse(c4.SetWinner());
+		
+		/**Valid Test Cases*/
 		c4.Move(9,6,"red");
 		c4.Move(9,6,"red");
 		c4.Move(9,6,"red");
 		c4.Move(9,6,"red");
 		assertTrue(c4.SetWinner());
+	}
+	
+	@Test
+	public void testGetPiece(){
+		assertEquals(null ,c4.GetPiece(2, 2));
+		c4.Move(9,3,"red");
+		assertEquals("red" ,c4.GetPiece(9, 6).GetColour());
+	}
+	
+	@Test
+	public void testGetWinningColour(){
+		assertThat("red", not(c4.GetWinningColour()));
+		c4.Move(9,6,"red");
+		c4.Move(9,6,"red");
+		c4.Move(9,6,"red");
+		c4.Move(9,6,"red");
+		c4.SetWinner();
+		assertEquals("red", c4.GetWinningColour());
+	}
+	
+	@Test
+	public void testGetTotalPieces(){
+		assertEquals(0, c4.GetTotalPieces());
+		c4.Move(9,6,"red");
+		c4.Move(9,6,"red");
+		assertEquals(2, c4.GetTotalPieces());
 	}
 
 }
